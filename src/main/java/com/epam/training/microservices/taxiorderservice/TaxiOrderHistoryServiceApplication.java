3692faddb4475cafe777eeec85ffd6ce143c6d6f
Maksim Makarenko <maksim_makarenko@epam.com>
2021-12-06 17:47:25 +0300

    add eureka

--- src/main/java/com/epam/training/microservices/taxiorderservice/TaxiOrderHistoryServiceApplication.java
+++ src/main/java/com/epam/training/microservices/taxiorderservice/TaxiOrderHistoryServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableKafka
 public class TaxiOrderHistoryServiceApplication {
 
